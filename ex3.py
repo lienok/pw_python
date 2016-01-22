5572c34a6f17531f7dd41a059f112dbbf9c2b251
Laurin <lienok@gmail.com>
2016-01-22 21:17:58 +0100

    small math

---
+++ ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chicken:"
+
+print "Kurky", 25 + 30 / 6
+print "deleni 75/4 = ", 75 / 4
+print "zvysok po deleni 75/4 = ", 75 % 4
+print "Kohutiky", 100 - 25 * 3 % 4 # % znamena zvysok po deleni cisla pred % cislom za %. Tzn. 10 deleno 3 = 3 a zvysok je 1 => vysledok je 1.
+
+print "Pocitam vajka:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6 #najskor sa pocitaju zatvorky, exponenty, nasobenie, delenie, sucet, rozdiel
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 7/4 # vznika nepresnost tym, ze vysledok sa nezaokruhluje, ale sa iba nezapise cast vysledku za desatinnou ciarkou. Zobrazi iba cele cisla.
+print 7.0/4.0 # presny vysledok
+print 10.0/3.0
+
+print "\je to pravda?"
+print 3 + 2 < 5 - 7
+print
+print "Kolko je 3+2?", 3 + 2
+print "Kolko je 5-7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Je to vacsie?", 5 > -2
+print "Is it greater or equal?", 5 >= -2 # = musi byt za sipkou
+print "Is it less or equal?", 5 <= -2
