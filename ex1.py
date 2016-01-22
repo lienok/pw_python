3151f217572d8ef15be8fc7c019582abab5e7e46
Laurin <lienok@gmail.com>
2016-01-22 19:41:00 +0100

    printing

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print "Hello world"
+print "Hello again"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print '*******Original***********'
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
